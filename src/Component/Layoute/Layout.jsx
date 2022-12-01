45f877948c1fb56822f68245ade4e2c0e37ffd94
Rizwan <eminence.rizwan@gmail.com>
2022-12-01 18:47:35 +0530

    add antd

--- src/Component/Layoute/Layout.jsx
+++ src/Component/Layoute/Layout.jsx
@@ -4,11 +4,41 @@ import Notfound from '../../Pages/Notfound';
 import { routepath } from '../../Router/RouteList';
 import SideBar from './SideBar';
 import ProtectRoutes from '../../Router/ProtectRoutes';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+// import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu } from 'antd';
 
-function Layout() {
+const { Header, Content, Sider } = Layout;
+
+const items1 = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  },
+);
+
+function Layoutes() {
     return (
         <div>
-            <SideBar />
+            {/* <SideBar />
             <Routes>
                 {routepath.map((i, index) => {
                     if (i.private) {
@@ -26,9 +56,60 @@ function Layout() {
                         );
                     }
                 })}
+            </Routes> */}
+              <Layout>
+    <Header className="header">
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+    </Header>
+    <Layout>
+      <Sider width={200} className="site-layout-background">
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100vh', borderRight: 0 }}
+          items={items2}
+        />
+      </Sider>
+      <Layout style={{ padding: '0 12px 12px' }}>
+        <Breadcrumb style={{ margin: '5px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            height: '100vh',
+          }}
+        >
+           <Routes>
+                {routepath.map((i, index) => {
+                    if (i.private) {
+                        return (
+                            <Route
+                                key={`routes_${index}`}
+                                path={i.path}
+                                // element={<i.Element />}
+                                element={<ProtectRoutes Component={i.Element } />} 
+                            />
+                        );
+                    } else {
+                        return (
+                            <Route key={`routes_${index}`} path="*" element={<Notfound />} />
+                        );
+                    }
+                })}
             </Routes>
+        </Content>
+      </Layout>
+    </Layout>
+  </Layout>
         </div>
     )
 }
 
-export default Layout;
+export default Layoutes;
--- src/Component/Layoute/SideBar.jsx
+++ src/Component/Layoute/SideBar.jsx
@@ -3,8 +3,9 @@ import { Link, NavLink } from "react-router-dom";
 
 function SideBar() {
   return (
-    <div> <div className="">
-    {/* <div className="">
+    <div> 
+      <div className="">
+    <div className="">
     <NavLink to="layout/link1">
             <div className="">
                 <p>link1</p>
@@ -22,62 +23,25 @@ function SideBar() {
                 <p>link3</p>
             </div>
         </NavLink>
-    </div> */}
-     <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style={{width:"280px"}}>
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <svg class="bi pe-none me-2" width="40" height="32"><use href="#bootstrap"/></svg>
-      <span class="fs-4">Sidebar</span>
-    </a>
-    <hr />
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi pe-none me-2" width="16" height="16"><use href="#home"/></svg>
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use href="#speedometer2"/></svg>
-          Dashboard
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use href="#table"/></svg>
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use href="#grid"/></svg>
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link text-white">
-          <svg class="bi pe-none me-2" width="16" height="16"><use href="#people-circle"/></svg>
-          Customers
-        </a>
-      </li>
-    </ul>
-    <hr />
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2" />
-        <strong>mdo</strong>
-      </a>
-      <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider" /></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
-      </ul>
     </div>
-  </div>
-</div></div>
+    
+</div>
+</div>
   )
 }
 
 export default SideBar
+// update  UI of Module Account pages ,
+// Add Suppler,
+// Add Consignor,
+// Add Consignee ,
+// Add unit
+// Add Vehicle,
+// and 
+// add custom validation in register page for email,
+
+// in Account Module 
+// pending points is 
+
+// Driver,
+// Vendors
